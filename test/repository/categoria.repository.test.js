fd78a32d77e1e09ee7cb257fca17d071bfeb094c
Gecen D De Marchi <jgecen@gmail.com>
2020-04-17 23:10:47 -0300

    Update Categoria

--- test/repository/categoria.repository.test.js
+++ test/repository/categoria.repository.test.js
@@ -1,6 +1,8 @@
 const knex = require("../../src/knex");
 const factory = require("../../src/repository/factory.repository");
 const categoriaRepository = factory.createRepositoryCategoria();
+let idForDelete = null;
+let categoriaForUpdate = null;
 
 describe("Teste de Repository de Categoria", () => {
   afterAll(async () => {
@@ -14,12 +16,34 @@ describe("Teste de Repository de Categoria", () => {
       { nome: "Categoria 2" },
       { nome: "Categoria 3" }
     ]);
+
+    const respCategoriaForDelete = await knex("categorias")
+      .returning(["id"])
+      .insert({ nome: "Categoria para teste de exclusão" });
+    idForDelete = respCategoriaForDelete.pop().id;
+
+    const respCategoriaForUpdate = await knex("categorias")
+      .returning("*")
+      .insert({ nome: "Categoria para alteração" });
+    categoriaForUpdate = respCategoriaForUpdate.pop();
   });
 
   test("testando a function save, sucesso", async () => {
     const data = await categoriaRepository.save({ nome: "Combustíveis" });
     expect(data).not.toBeNull();
   });
+
+  test("testando a function update, sucesso", async () => {
+    categoriaForUpdate.nome = "CATEGORIA_ALTERADA";
+    const data = await categoriaRepository.update(categoriaForUpdate);
+    expect(data.pop().nome).toBe(categoriaForUpdate.nome);
+  });
+
+  test("testando a function del, sucesso", async () => {
+    const respDelete = await categoriaRepository.del(idForDelete);
+    expect(respDelete.pop().id).toBe(idForDelete);
+  });
+
   test("testando a function save, erro constrainst", () => {
     categoriaRepository
       .save({ nome: "Folha de Pagamento" })
@@ -30,6 +54,7 @@ describe("Teste de Repository de Categoria", () => {
         expect(err).not.toBeNull();
       });
   });
+
   test("testando a function getAll", () => {
     categoriaRepository
       .getAll()
