11a1f2299184c938814aa452a645edd4f4000b2f
Gecen D De Marchi <jgecen@gmail.com>
2020-04-21 20:02:31 -0300

    Padronização das respostas

--- src/controller/empresa.controller.js
+++ src/controller/empresa.controller.js
@@ -1,22 +1,22 @@
 const { validationResult } = require("express-validator");
 const createEmpresaSerice = require("../service/create.empresa.service");
-const createDefaultController = () => {
+const tratarRespostas = require("./respostas");
+
+const createEmpresaController = () => {
   let _empresaService = createEmpresaSerice();
 
   const _post = (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
+      tratarRespostas.resposta422(res, errors);
     } else {
       _empresaService
         .save(req.params.urn, req.body)
         .then((data) => {
-          res.status(201);
-          res.send({ message: "Recurso criado com sucesso!", recurso: data });
+          tratarRespostas.resposta201(res, data);
         })
-        .catch((err) => {
-          res.status(412);
-          res.send({ erro: err.detail });
+        .catch((error) => {
+          tratarRespostas.resposta412(res, error);
         });
     }
   };
@@ -26,4 +26,4 @@ const createDefaultController = () => {
   };
 };
 
-module.exports = createDefaultController;
+module.exports = createEmpresaController;
